534aeb326350973399fbe036e43366e527a9f083
Emanuel Tannert <post@etannert.de>
2020-01-27 17:41:40 +0100

    let black format connection.py

--- wpsync/connection.py
+++ wpsync/connection.py
@@ -11,9 +11,9 @@ import requests
 
 @contextmanager
 def connect(site):
-    if site['protocol'] == 'file':
+    if site["protocol"] == "file":
         connection = FileConnection(site)
-    elif site['protocol'] in ['ftp', 'sftp']:
+    elif site["protocol"] in ["ftp", "sftp"]:
         connection = FTPConnection(site)
     else:
         connection = SSHConnection(site)
@@ -36,10 +36,10 @@ class RemoteExecutionError(Exception):
 class Connection:
     def __init__(self, site):
         self.site = site
-        self.wpsync_dir = site['base_dir'] + 'wpsync'
+        self.wpsync_dir = site["base_dir"] + "wpsync"
 
     def normalise(self, path):
-        return f'{self.wpsync_dir}/{s(path)}'
+        return f"{self.wpsync_dir}/{s(path)}"
 
     def make_wpsync_dir(self):
         # TODO better to ask forgiveness
@@ -50,14 +50,14 @@ class Connection:
         self.rmdir(self.wpsync_dir)
 
     def run_php(self, php_code):
-        path = self.normalise('run.php')
-        url = self.site['base_url']
-        if url[-1] != '/':
-            url += '/'
-        url += 'wpsync/run.php'
+        path = self.normalise("run.php")
+        url = self.site["base_url"]
+        if url[-1] != "/":
+            url += "/"
+        url += "wpsync/run.php"
         self.cat_r(path, php_code)
-        if 'http_user' in self.site:
-            auth = (self.site['http_user'], self.site['http_pass'])
+        if "http_user" in self.site:
+            auth = (self.site["http_user"], self.site["http_pass"])
         else:
             auth = None
 
@@ -101,22 +101,27 @@ class FileConnection(Connection):
         shutil.copyfile(local_path, remote_path)
 
     def mirror(self, remote_path, local_path):
-        rsync('--recursive', '--del', '--compress',
-              remote_path + '/', s(local_path))
+        rsync(
+            "--recursive",
+            "--del",
+            "--compress",
+            remote_path + "/",
+            s(local_path),
+        )
 
     def mirror_r(self, local_path, remote_path, exclude=[]):
-        args = ['--recursive', '--del', '--compress']
+        args = ["--recursive", "--del", "--compress"]
         for pattern in exclude:
-            args.append(f'--exclude={quote(pattern)}')
-        args.extend([s(local_path) + '/', s(remote_path)])
+            args.append(f"--exclude={quote(pattern)}")
+        args.extend([s(local_path) + "/", s(remote_path)])
         rsync(*args)
 
     def cat(self, path):
-        with open(path, 'r') as f:
+        with open(path, "r") as f:
             return f.read()
 
     def cat_r(self, path, string):
-        with open(path, 'w') as f:
+        with open(path, "w") as f:
             return f.write(string)
 
     def rm(self, path):
@@ -126,124 +131,130 @@ class FileConnection(Connection):
 class SSHConnection(Connection):
     def __init__(self, site):
         super().__init__(site)
-        self.user = quote(site['user'])
-        self.host = quote(site['host'])
+        self.user = quote(site["user"])
+        self.host = quote(site["host"])
 
     def ssh_do(self, command):
-        return ssh(f'{self.user}@{self.host}', command)
+        return ssh(f"{self.user}@{self.host}", command)
 
     def dir_exists(self, path):
         try:
-            res = self.ssh_do(f'test -d {quote(s(path))} && echo yes')
+            res = self.ssh_do(f"test -d {quote(s(path))} && echo yes")
         except ErrorReturnCode_1 as e:
             return False
-        return 'yes' in res
+        return "yes" in res
 
     def file_exists(self, path):
         try:
-            res = self.ssh_do(f'test -f {quote(s(path))} && echo yes')
+            res = self.ssh_do(f"test -f {quote(s(path))} && echo yes")
         except ErrorReturnCode_1 as e:
             return False
-        return 'yes' in res
+        return "yes" in res
 
     def mkdir(self, path):
-        self.ssh_do(f'mkdir {quote(s(path))}')
+        self.ssh_do(f"mkdir {quote(s(path))}")
 
     def rmdir(self, path):
-        self.ssh_do(f'rm -r {quote(s(path))}')
+        self.ssh_do(f"rm -r {quote(s(path))}")
 
     def get(self, remote_path, local_path):
-        scp(f'{self.user}@{self.host}:{quote(s(remote_path))}', s(local_path))
+        scp(f"{self.user}@{self.host}:{quote(s(remote_path))}", s(local_path))
 
     def put(self, local_path, remote_path):
-        scp(local_path, f'{self.user}@{self.host}:{quote(s(remote_path))}')
+        scp(local_path, f"{self.user}@{self.host}:{quote(s(remote_path))}")
 
     def mirror(self, remote_path, local_path):
-        rsync('--recursive', '--del', '--compress',
-              f'{self.user}@{self.host}:{quote(s(remote_path))}/',
-              s(local_path))
+        rsync(
+            "--recursive",
+            "--del",
+            "--compress",
+            f"{self.user}@{self.host}:{quote(s(remote_path))}/",
+            s(local_path),
+        )
 
     def mirror_r(self, local_path, remote_path, exclude=[]):
-        args = ['--recursive', '--del', '--compress']
+        args = ["--recursive", "--del", "--compress"]
         for pattern in exclude:
-            args.append(f'--exclude={quote(pattern)}')
-        args.append(s(local_path) + '/')
-        args.append(f'{self.user}@{self.host}:{quote(s(remote_path))}')
+            args.append(f"--exclude={quote(pattern)}")
+        args.append(s(local_path) + "/")
+        args.append(f"{self.user}@{self.host}:{quote(s(remote_path))}")
         rsync(*args)
 
     def cat(self, path):
-        return self.ssh_do(f'cat {quote(s(path))}')
+        return self.ssh_do(f"cat {quote(s(path))}")
 
     def cat_r(self, path, string):
-        self.ssh_do(f'echo {quote(string)} > {quote(s(path))}')
+        self.ssh_do(f"echo {quote(string)} > {quote(s(path))}")
 
     def rm(self, path):
-        self.ssh_do(f'rm {quote(s(path))}')
+        self.ssh_do(f"rm {quote(s(path))}")
 
 
 class FTPConnection(Connection):
     def __init__(self, site):
         super().__init__(site)
-        self.user = quote(site['user'])
-        self.pasw = quote(site['pass'])
-        self.host = quote(site['host'])
+        self.user = quote(site["user"])
+        self.pasw = quote(site["pass"])
+        self.host = quote(site["host"])
 
     def lftp(self, command, capture=False):
         completed_process = run(
             [
-                'lftp',
-                '-c',
-                f'open -u {self.user},{self.pasw} {self.host}; {command}; quit'
+                "lftp",
+                "-c",
+                f"open -u {self.user},{self.pasw} {self.host}; {command}; quit",
             ],
-            capture_output=capture
+            capture_output=capture,
         )
         if completed_process.stdout:
-            return completed_process.stdout.decode('utf8')
-        return ''
+            return completed_process.stdout.decode("utf8")
+        return ""
 
     def dir_exists(self, path):
-        path = path[:-1] + '[' + path[-1] + ']'
-        res = self.lftp(f'glob --exist -d {quote(s(path))} && echo yes',
-                        capture=True)
-        return 'yes' in res
+        path = path[:-1] + "[" + path[-1] + "]"
+        res = self.lftp(
+            f"glob --exist -d {quote(s(path))} && echo yes", capture=True
+        )
+        return "yes" in res
 
     def file_exists(self, path):
-        path = path[:-1] + '[' + path[-1] + ']'
-        res = self.lftp(f'glob --exist -f {quote(s(path))} && echo yes',
-                        capture=True)
-        return 'yes' in res
+        path = path[:-1] + "[" + path[-1] + "]"
+        res = self.lftp(
+            f"glob --exist -f {quote(s(path))} && echo yes", capture=True
+        )
+        return "yes" in res
 
     def mkdir(self, path):
-        self.lftp(f'mkdir -p {quote(s(path))}')
+        self.lftp(f"mkdir -p {quote(s(path))}")
 
     def rmdir(self, path):
-        self.lftp(f'rm -r {quote(s(path))}')
+        self.lftp(f"rm -r {quote(s(path))}")
 
     def get(self, remote_path, local_path):
-        self.lftp(f'get {quote(s(remote_path))} -o {quote(s(local_path))}')
+        self.lftp(f"get {quote(s(remote_path))} -o {quote(s(local_path))}")
 
     def put(self, local_path, remote_path):
-        self.lftp(f'put {quote(s(local_path))} -o {quote(s(remote_path))}')
+        self.lftp(f"put {quote(s(local_path))} -o {quote(s(remote_path))}")
 
     def mirror(self, remote_path, local_path):
-        cmd = 'mirror --delete'
-        self.lftp(f'{cmd} {quote(s(remote_path))} {quote(s(local_path))}')
+        cmd = "mirror --delete"
+        self.lftp(f"{cmd} {quote(s(remote_path))} {quote(s(local_path))}")
 
     def mirror_r(self, local_path, remote_path, exclude=[]):
-        cmd = 'mirror --delete -R'
+        cmd = "mirror --delete -R"
         for pattern in exclude:
-            cmd += f' --exclude {quote(pattern)}'
-        cmd += f' {quote(s(local_path))} {quote(s(remote_path))}'
+            cmd += f" --exclude {quote(pattern)}"
+        cmd += f" {quote(s(local_path))} {quote(s(remote_path))}"
         self.lftp(cmd)
 
     def cat(self, path):
-        return self.lftp(f'cat {quote(s(path))}', capture=True)
+        return self.lftp(f"cat {quote(s(path))}", capture=True)
 
     def cat_r(self, path, string):
         tmp_file = Path(NamedTemporaryFile().name)
-        tmp_file.write_text(string, encoding='utf-8')
-        self.lftp(f'put {quote(s(tmp_file))} -o {quote(s(path))}')
+        tmp_file.write_text(string, encoding="utf-8")
+        self.lftp(f"put {quote(s(tmp_file))} -o {quote(s(path))}")
         tmp_file.unlink()
 
     def rm(self, path):
-        self.lftp(f'rm {quote(s(path))}')
+        self.lftp(f"rm {quote(s(path))}")
