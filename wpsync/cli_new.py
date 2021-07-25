36ca5f2111b8c05a65b923b89dd990d522d129d9
Emanuel Tannert <post@etannert.de>
2021-07-25 15:14:55 +0200

    first attempt at new cli with fewer dependencies

---
+++ wpsync/cli_new.py
@@ -0,0 +1,432 @@
+from argparse import ArgumentParser, Namespace
+from sys import argv, exit
+from typing import List, NamedTuple, Optional
+
+
+WPSYNC_VERSION = "2.0.1"
+
+
+MYSQLDUMP_URL = "https://raw.githubusercontent.com/ifsnop/mysqldump-php/v2.9/src/Ifsnop/Mysqldump/Mysqldump.php"
+
+
+def main(argv: List[str] = None):
+    dings = SiteConfiguration(site_name="wurm")
+
+    main_parser = ArgumentParser(
+        prog="wpsync",
+        description="Synchronise WordPress sites across ssh, (s)ftp and local hosts",
+        allow_abbrev=False,
+    )
+    main_parser.add_argument(
+        "-V", "--version", action="store_true", help="output version number"
+    )
+    # NOTE I never use this
+    main_parser.add_argument(
+        "-q",
+        "--quiet",
+        action="store_true",
+        help="don't print anything to STDOUT",
+    )
+    # NOTE I never use this
+    main_parser.add_argument(
+        "-c", "--config", help="use the config file specified"
+    )
+
+    subparsers = main_parser.add_subparsers()
+
+    sync_parser = subparsers.add_parser(
+        "sync", aliases=("s"), help="sync one installation to another"
+    )
+    sync_parser.add_argument(
+        "-d", "--database", action="store_true", help="sync the database"
+    )
+    sync_parser.add_argument(
+        "-u", "--uploads", action="store_true", help="sync the uploads folder"
+    )
+    sync_parser.add_argument(
+        "-p",
+        "--plugins",
+        action="store_true",
+        help="sync the plugins directory",
+    )
+    sync_parser.add_argument(
+        "-t", "--themes", action="store_true", help="sync the themes"
+    )
+    sync_parser.add_argument(
+        "-a", "--all", action="store_true", help="sync all the things"
+    )
+    sync_parser.add_argument(
+        "-f",
+        "--full",
+        action="store_true",
+        help="sync the database and the complete WordPress installation",
+    )
+    sync_parser.set_defaults(func=sync)
+
+    backup_parser = subparsers.add_parser(
+        "backup", aliases=("b"), help="create a backup of an installation"
+    )
+    backup_parser.add_argument(
+        "-d", "--database", action="store_true", help="backup the database"
+    )
+    backup_parser.add_argument(
+        "-u",
+        "--uploads",
+        action="store_true",
+        help="backup the uploads folder",
+    )
+    backup_parser.add_argument(
+        "-p",
+        "--plugins",
+        action="store_true",
+        help="backup the plugins directory",
+    )
+    backup_parser.add_argument(
+        "-t", "--themes", action="store_true", help="backup the themes"
+    )
+    backup_parser.add_argument(
+        "-a", "--all", action="store_true", help="backup all the things"
+    )
+    backup_parser.add_argument(
+        "-f",
+        "--full",
+        action="store_true",
+        help="backup the database and the complete WordPress installation",
+    )
+    backup_parser.set_defaults(func=backup)
+
+    restore_parser = subparsers.add_parser(
+        "restore", aliases=("r"), help="restore a backup into an installation"
+    )
+    restore_parser.add_argument(
+        "-d", "--database", action="store_true", help="restore the database"
+    )
+    restore_parser.add_argument(
+        "-u",
+        "--uploads",
+        action="store_true",
+        help="restore the uploads folder",
+    )
+    restore_parser.add_argument(
+        "-p",
+        "--plugins",
+        action="store_true",
+        help="restore the plugins directory",
+    )
+    restore_parser.add_argument(
+        "-t", "--themes", action="store_true", help="restore the themes"
+    )
+    restore_parser.add_argument(
+        "-a", "--all", action="store_true", help="restore all the things"
+    )
+    restore_parser.add_argument(
+        "-f",
+        "--full",
+        action="store_true",
+        help="restore the database and the complete WordPress installation",
+    )
+    restore_parser.set_defaults(func=restore)
+
+    list_parser = subparsers.add_parser(
+        "list", aliases=("l"), help="list existing backups"
+    )
+    list_parser.add_argument(
+        "-d",
+        "--database",
+        action="store_true",
+        help="list backups containing the database",
+    )
+    list_parser.add_argument(
+        "-u",
+        "--uploads",
+        action="store_true",
+        help="list backups containing the uploads folder",
+    )
+    list_parser.add_argument(
+        "-p",
+        "--plugins",
+        action="store_true",
+        help="list backups containing the plugins directory",
+    )
+    list_parser.add_argument(
+        "-t",
+        "--themes",
+        action="store_true",
+        help="list backups containing the themes",
+    )
+    list_parser.add_argument(
+        "-a",
+        "--all",
+        action="store_true",
+        help="list backups containing all of these things",
+    )
+    list_parser.add_argument(
+        "-f",
+        "--full",
+        action="store_true",
+        help="list full backups",
+    )
+    list_parser.set_defaults(func=list_backups)
+
+    install_parser = subparsers.add_parser(
+        "install", aliases=("i"), help="install new WordPress at site"
+    )
+    install_parser.set_defaults(func=install)
+
+    args = main_parser.parse_args()
+
+    if args.version:
+        print("v" + WPSYNC_VERSION)
+        exit()
+
+    # TODO of course remove this print and also validate the
+    # main arguments ("options"?)
+    print(args)
+
+    # TODO check existence of external executables
+    # (or eliminate need for them)
+    # - but maybe do it later when we know exactly which we're
+    # going to need for the command executed?
+
+    # TODO get config
+    # TODO validate config
+
+    # TODO find the wpsync dir
+
+    # TODO whatever `get_options` used to do?
+
+    # TODO convert config: copy aliased entries --
+    # or maybe do this after loading?
+    # >> also bail if an alias would overwrite an existing entry!!!
+
+    # TODO add config, config path, wpsyncdir and "options" to args
+    # (or use another way to pass them to the function)
+
+    if "func" not in args:
+        # TODO bail if no subcommand was called --
+        # I guess args.func wouldn't be set then?
+        print("y u no call subcommand")
+        exit()
+    args.func(args)
+    # TODO in the jeweilig functions:
+    # validate --arguments (combinations of them)
+    # interpret positional args
+
+
+def cli_sync(args: Namespace) -> None:
+    print("-> ZYNC")
+
+
+def cli_backup(args: Namespace) -> None:
+    print("--- BACKup")
+
+
+def cli_restore(args: Namespace) -> None:
+    print("Re[store]")
+
+
+def cli_list_backups(args: Namespace) -> None:
+    print("LiBa")
+
+
+def cli_install(args: Namespace) -> None:
+    print("insti insti clik klikc")
+
+
+##########
+# CONFIG #
+##########
+
+
+class Config:
+    config_file_names = [
+        "wpsync.ini",
+        "wpsync.config.ini",
+        ".wpsyncrc",
+        ".wpsync.ini",
+        ".wpsync.config.ini",
+        ".wpsync/wpsync.ini",
+        ".wpsync/wpsync.config.ini",
+    ]
+
+    @staticmethod
+    def read_file(path):
+        with open(path) as f:
+            return f.read()
+
+
+def get_config(path):
+    config_raw = None
+
+    if path is not None:
+        try:
+            config_raw = read_file(path)
+        except FileNotFoundError:
+            raise ConfigurationError(f"No config file found at {path}")
+    else:
+        # make a list of possible config file paths
+        cwd = Path.cwd()
+        home = Path.home()
+        dirs = [d for d in [cwd, *cwd.parents] if d not in home.parents]
+        if home not in dirs:
+            dirs.append(home)
+        for path in (d / f for d in dirs for f in config_file_names):
+            try:
+                config_raw = read_file(path)
+                break
+            except FileNotFoundError:
+                pass
+
+    if config_raw is None:
+        raise ConfigurationError("No config file found")
+
+    config = ConfigParser()
+    config.read(config_raw)
+    # TODO validate config
+    # defaults = validate_config_default(config._defaults)
+    # valid_config = validate_config_sections(config._sections)
+    # normalized_config = normalize_config(valid_config, defaults)
+    return (normalized_config, p)
+
+
+class ConfigurationError(Exception):
+    pass
+
+
+class SiteNameConfiguration(NamedTuple):
+    name: str
+    alias: Optional[str] = None
+
+
+class URLConfiguration:
+    def __init__(self, site_url: str, file_url: Optional[str] = None):
+        self._site_url = self.trim_trailing_slashes(site_url)
+        if file_url is None:
+            self._file_url = site_url
+        else:
+            self._file_url = self.trim_trailing_slashes(file_url)
+
+    @property
+    def site_url(self):
+        return self._site_url
+
+    @property
+    def file_url(self):
+        return self._file_url
+
+    @staticmethod
+    def trim_trailing_slashes(url):
+        while len(url) and url[-1] == "/":
+            url = url[:-1]
+        return url
+
+
+class BaseDirConfiguration:
+    def __init__(self, base_dir: str):
+        while len(base_dir) and base_dir[-1] == "/":
+            base_dir = base_dir[:-1]
+        self._base_dir = base_dir
+
+    @property
+    def base_dir(self):
+        return self._base_dir
+
+
+class ConnectionConfiguration:
+    def __init__(
+        self,
+        protocol: str,
+        username: Optional[str] = None,
+        host: Optional[str] = None,
+        password: Optional[str] = None,
+    ):
+        if protocol == "file":
+            if (
+                username is not None
+                or host is not None
+                or password is not None
+            ):
+                raise ConfigurationError(
+                    "Can't use user/host/password with file protocol"
+                )
+        elif protocol == "ftp" or protocol == "sftp":
+            if username is None:
+                raise ConfigurationError(f"Specify {protocol} user")
+            if host is None:
+                raise ConfigurationError(f"Specify {protocol} host")
+            if password is None:
+                raise ConfigurationError(f"Specify {protocol} password")
+        elif protocol == "ssh":
+            if username is None:
+                raise ConfigurationError("Specify ssh user")
+            if host is None:
+                raise ConfigurationError("Specify ssh host")
+            if password is not None:
+                raise ConfigurationError(
+                    "SSH with password is not supported;"
+                    " Set up public key authentication or use sftp"
+                )
+        else:
+            raise ConfigurationError(f"Unknwon protocol: {protocol}")
+
+        self._protocol = protocol
+        self._username = username
+        self._host = host
+        self._password = password
+
+    @property
+    def protocol(self):
+        return self._protocol
+
+    @property
+    def username(self):
+        return self._username
+
+    @property
+    def host(self):
+        return self._host
+
+    @property
+    def password(self):
+        return self._password
+
+
+class DatabaseConfiguration(NamedTuple):
+    name: str
+    host: str
+    username: str
+    password: str
+    port: Optional[str] = None
+    prefix: str = "wp_"
+
+
+class HttpBasicAuthConfiguration(NamedTuple):
+    username: str
+    password: str
+
+
+class MisbehavedHostConfiguration(NamedTuple):
+    no_verify_ssl: bool = False
+    sudo_remote: bool = False
+    chown_remote: Optional[str] = None
+    chgrp_remote: Optional[str] = None
+
+
+class SiteConfiguration(NamedTuple):
+    site_name: SiteNameConfiguration
+    url: URLConfiguration
+    base_dir: BaseDirConfiguration
+    connection: ConnectionConfiguration
+    database: DatabaseConfiguration
+    http_basic_auth: Optional[HttpBasicAuthConfiguration] = None
+    misbehaved_host: MisbehavedHostConfiguration = (
+        MisbehavedHostConfiguration()
+    )
+
+
+def backup(installation):
+    pass
+
+
+if __name__ == "__main__":
+    main(argv)
