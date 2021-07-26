2ca2f8d6d0f71b25098362ab97a082432b8bb477
Emanuel Tannert <post@etannert.de>
2021-07-26 10:31:31 +0200

    test more command line options

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -17,3 +17,32 @@ class TestCLI(unittest.TestCase):
         self.assertEqual(config["print_config"], True)
         self.assertEqual(config["quiet"], False)
         self.assertEqual(config["version"], False)
+
+    def test_print_config_version(self):
+        with contextlib.redirect_stdout(io.StringIO()) as f:
+            wpsync.cli_new.main(["--print-config", "--version"])
+        config = json.loads(f.getvalue())
+        self.assertEqual(config["config"], None)
+        self.assertEqual(config["print_config"], True)
+        self.assertEqual(config["quiet"], False)
+        self.assertEqual(config["version"], True)
+
+    def test_print_config_config(self):
+        with contextlib.redirect_stdout(io.StringIO()) as f:
+            wpsync.cli_new.main(
+                ["--print-config", "--config", "notexist.ini"]
+            )
+        config = json.loads(f.getvalue())
+        self.assertEqual(config["config"], "notexist.ini")
+        self.assertEqual(config["print_config"], True)
+        self.assertEqual(config["quiet"], False)
+        self.assertEqual(config["version"], False)
+
+    def test_print_config_quiet(self):
+        with contextlib.redirect_stdout(io.StringIO()) as f:
+            wpsync.cli_new.main(["--print-config", "--quiet"])
+        config = json.loads(f.getvalue())
+        self.assertEqual(config["config"], None)
+        self.assertEqual(config["print_config"], True)
+        self.assertEqual(config["quiet"], True)
+        self.assertEqual(config["version"], False)
