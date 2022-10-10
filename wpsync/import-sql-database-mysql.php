0bbf33e2b92fb1795af0822c780edab10be7a3e1
Emanuel Tannert <post@etannert.de>
2022-10-10 18:25:14 +0200

    read db dumps line by line to save ram etc.,
    this way memory limits etc. won't have to be turned up in php.ini to
    make import of large databases work
    
    also generally clean up the code

--- wpsync/import-sql-database-mysql.php
+++ wpsync/import-sql-database-mysql.php
@@ -1,21 +1,36 @@
 <?php	  
-	// EXAMPLE:	IMPORT_TABLES("localhost","user","pass","db_name", "my_baseeee.sql"); //TABLES WILL BE OVERWRITTEN
-				// P.S. IMPORTANT NOTE for people who try to change/replace some strings  in SQL FILE before importing, MUST READ:  https://goo.gl/2fZDQL
+// EXAMPLE: IMPORT_TABLES("localhost","user","pass","db_name", "my_baseeee.sql"); //TABLES WILL BE OVERWRITTEN
+// P.S. IMPORTANT NOTE for people who try to change/replace some strings in SQL FILE before importing, MUST READ: https://goo.gl/2fZDQL
 
 // https://github.com/tazotodua/useful-php-scripts 
-function IMPORT_TABLES($host,$user,$pass,$dbname,$port, $sql_file_OR_content){
-	set_time_limit(3000);
-	$SQL_CONTENT = (strlen($sql_file_OR_content) > 300 ?  $sql_file_OR_content : file_get_contents($sql_file_OR_content)  );  
-	$allLines = explode("\n",$SQL_CONTENT); 
-	$mysqli = new mysqli($host, $user, $pass, $dbname, $port); if (mysqli_connect_errno()){echo "Failed to connect to MySQL: " . mysqli_connect_error();} 
-		$zzzzzz = $mysqli->query('SET foreign_key_checks = 0');	        preg_match_all("/\nCREATE TABLE(.*?)\`(.*?)\`/si", "\n". $SQL_CONTENT, $target_tables); foreach ($target_tables[2] as $table){$mysqli->query('DROP TABLE IF EXISTS '.$table);}         $zzzzzz = $mysqli->query('SET foreign_key_checks = 1');    $mysqli->query("SET NAMES 'utf8'");	
-	$templine = '';	// Temporary variable, used to store current query
-	foreach ($allLines as $line)	{											// Loop through each line
-		if (substr($line, 0, 2) != '--' && $line != '') {$templine .= $line; 	// (if it is not a comment..) Add this line to the current segment
-			if (substr(trim($line), -1, 1) == ';') {		// If it has a semicolon at the end, it's the end of the query
-				if(!$mysqli->query($templine)){ print('Error performing query \'<strong>' . $templine . '\': ' . $mysqli->error . '<br /><br />');  }  $templine = ''; // set variable to empty, to start picking up the lines after ";"
-			}
-		}
-	}	return 'Importing finished. Now, Delete the import file.';
-}   //see also export.php 
-?>
+function IMPORT_TABLES($host,$user,$pass,$dbname,$port, $sql_file_path) {
+    set_time_limit(3000);
+    $handle = fopen($sql_file_path, 'r');
+
+    if (!$handle) {
+        echo "Failed to open {$sql_file_path}\n";
+        return;
+    }
+
+    $mysqli = new mysqli($host, $user, $pass, $dbname, $port);
+    if (mysqli_connect_errno()) {
+        echo 'Failed to connect to MySQL: ' . mysqli_connect_error() . "\n";
+        return;
+    }
+
+    $mysqli->query("SET NAMES 'utf8'");
+    $templine = '';	// Temporary variable, used to store current query
+
+    while (($line = fgets($handle)) !== false) {
+        if (substr($line, 0, 2) != '--' && $line != '') {
+            $templine .= $line; // (if it is not a comment..) Add this line to the current segment
+            if (substr(trim($line), -1, 1) == ';') { // If it has a semicolon at the end, it's the end of the query
+                if (!$mysqli->query($templine)) {
+                    print('Error performing query \'<strong>' . $templine . '\': ' . $mysqli->error . '<br /><br />' . "\n");
+                }
+                $templine = ''; // set variable to empty, to start picking up the lines after ";"
+            }
+        }
+    }
+    return 'Importing finished. Now, Delete the import file.';
+}
